0d95b1302dc2a0556c8deeddc3416146c063c521
AnshuJalan <anshujalan206@gmail.com>
2022-05-13 14:51:28 +0530

    fix: empty stats collection error

--- indexer/src/server/routers/Stats.ts
+++ indexer/src/server/routers/Stats.ts
@@ -5,17 +5,21 @@ const build = (databaseClient: DatabaseClient): Router => {
   const router = Router();
 
   router.get("/", async (_, res: Response) => {
-    const sizzlersAggr = await databaseClient.models.sizzler.aggregate([
-      { $group: { _id: null, sizzlers: { $sum: 1 }, totalTasksLimit: { $sum: "$taskLimit" } } },
-    ]);
-    delete sizzlersAggr[0]._id;
+    try {
+      const sizzlersAggr = await databaseClient.models.sizzler.aggregate([
+        { $group: { _id: null, sizzlers: { $sum: 1 }, totalTasksLimit: { $sum: "$taskLimit" } } },
+      ]);
+      delete sizzlersAggr[0]._id;
 
-    const tasksAggr = await databaseClient.models.task.aggregate([
-      { $group: { _id: null, activeTasks: { $sum: 1 } } },
-    ]);
-    delete tasksAggr[0]._id;
+      const tasksAggr = await databaseClient.models.task.aggregate([
+        { $group: { _id: null, activeTasks: { $sum: 1 } } },
+      ]);
+      delete tasksAggr[0]._id;
 
-    res.json({ ...sizzlersAggr[0], ...tasksAggr[0] }).status(200);
+      res.json({ ...sizzlersAggr[0], ...tasksAggr[0] }).status(200);
+    } catch (err) {
+      res.json({ sizzlers: 0, totalTasksLimit: 0, activeTasks: 0 });
+    }
   });
 
   return router;
